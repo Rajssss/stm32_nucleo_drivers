0526044fe573b8f2f6ec36e5086c024c10419a91
Rajssss <sssraj.sssraj@gmail.com>
2020-06-23 00:08:46 +0530

    I2C Driver Update: Added Clock Control and Init handler
    Signed-off-by: Rajssss <sssraj.sssraj@gmail.com>

--- stm32f4xx_driver/driver/src/stm32f446xx_i2c_driver.c
+++ stm32f4xx_driver/driver/src/stm32f446xx_i2c_driver.c
@@ -22,8 +22,132 @@
  *      Description: I2C Driver for STM32F446RE-Nucleo Board
  *
  *      This file contains:
- *           - Function definitions to perform various operation on a GPIO Port or Pin.
+ *           - Function definitions to perform various operation on I2C Peripheral.
  *
  *
  */
+
+//includes
+#include "stm32f446xx_i2c_driver.h"
+
+
+
+/***********************************************************************************
+ * 					 	I2C Clock Control Handler
+ *
+ * @fn: 		- 	I2Cx_PeriClkControl
+ *
+ * @brief		-	This function Deinitialize the given I2C Port
+ *
+ * @param[1]	-	Base Address of the I2C Peripheral
+ *
+ * @param[2]	-	Control value: ENABLE or DISABLE
+ *
+ * @return		-	void
+ *
+ * @Note		-	none
+ *
+ */
+void I2Cx_PeriClkControl(I2Cx_RegDef_ty *pI2Cx, uint8_t Control)
+{
+	if(Control == ENABLE)
+			{
+				if(pI2Cx == I2C1)
+				{
+					I2C1_PCLK_EN();
+				}
+				else if(pI2Cx == I2C2)
+				{
+					I2C2_PCLK_EN();
+				}
+				else if(pI2Cx == I2C3)
+				{
+					I2C3_PCLK_EN();
+				}
+
+			}
+			else
+			{
+				if(pI2Cx == I2C1)
+				{
+					I2C1_PCLK_DI();
+				}
+				else if(pI2Cx == I2C2)
+				{
+					I2C2_PCLK_DI();
+				}
+				else if(pI2Cx == I2C3)
+				{
+					I2C3_PCLK_DI();
+				}
+
+			}
+
+
+}
+
+
+
+
+
+/***********************************************************************************
+ * 					 	  	 I2C Initialize Handler
+ *
+ * @fn: 		- 	I2Cx_Init
+ *
+ * @brief		-	This function will configure the various Registers associated with that
+ * 					I2C Peripheral with the values present in I2Cx_Config structure.
+ *
+ * @param[1]	-	Address of the I2C Handler Request.
+ *
+ * @return		-	void
+ *
+ * @Note		-	TODO: 1. Dual Own Address Mode
+ *
+ */
+void I2Cx_Init(I2Cx_Handler_ty *pI2CHandler)
+{
+	//ACK Control
+	pI2CHandler->pI2Cx->CR1 |= (pI2CHandler->I2Cx_Config.I2C_ACKControl << I2C_CR1_ACK);
+
+	//Frequency
+	 pI2CHandler->pI2Cx->CR2 |= (RCC_GetPeriCLK1_Value() / 1000000U & 0x3F);
+
+
+	 //Primary Device Address
+	 if(pI2CHandler->I2Cx_Config.I2C_DeviceAddrMode == I2C_DeviceAddr_10BIT)
+	 {
+		 pI2CHandler->pI2Cx->OAR1 |= ((0x3 << 14)  + pI2CHandler->I2Cx_Config.I2C_DeviceAddress_Primary);
+	 }
+	 else
+	 {
+		 pI2CHandler->pI2Cx->OAR1 |= ((0x1 << 14)  + (pI2CHandler->I2Cx_Config.I2C_DeviceAddress_Primary << 1));
+	 }
+
+	 //Dual Address Mode
+	 if(pI2CHandler->I2Cx_Config.I2C_DeviceAddrMode == I2C_DeviceAddr_Dual)
+	 {
+		 pI2CHandler->pI2Cx->OAR2 |= ((pI2CHandler->I2Cx_Config.I2C_DeviceAddress_Secondary << 1) + 0x1 );
+	 }
+
+	 //CCR
+	 if(pI2CHandler->I2Cx_Config.I2C_SCLSpeed <= I2C_SCL_SPEED_STD)			//Standard Mode
+	 {
+		 pI2CHandler->pI2Cx->CCR |= ((RCC_GetPeriCLK1_Value() / (2 * pI2CHandler->I2Cx_Config.I2C_SCLSpeed)) & 0xFFF);
+	 }
+	 else																	//Fast Mode
+	 {
+		 if(pI2CHandler->I2Cx_Config.I2C_FMDutyCycle == I2C_DUTY_2)
+		 {
+			 pI2CHandler->pI2Cx->CCR |= ((0x1 << 15) + (RCC_GetPeriCLK1_Value() / (3 * pI2CHandler->I2Cx_Config.I2C_SCLSpeed)));
+		 }
+		 else
+		 {
+			 pI2CHandler->pI2Cx->CCR |= ((0x3 << 14) + (RCC_GetPeriCLK1_Value() / (25 * pI2CHandler->I2Cx_Config.I2C_SCLSpeed)));
+		 }
+
+	 }
+
+}
+
 
