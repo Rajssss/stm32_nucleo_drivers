ac7c28396914dd341c13b0c3b0d63757ba470afa
Rajssss <sssraj.sssraj@gmail.com>
2020-07-09 23:28:36 +0530

    I2C Driver Update: Added Driver headers to HAL itself
    ->Now, just add stm32f446xx.h to your projects and build
    
    Signed-off-by: Rajssss <sssraj.sssraj@gmail.com>

--- stm32f4xx_driver/driver/inc/stm32f446xx.h
+++ stm32f4xx_driver/driver/inc/stm32f446xx.h
@@ -830,4 +830,11 @@ typedef struct
 
 
 
+//Reverse Include
+#include "stm32f446xx_gpio_driver.h"
+#include "stm32f446xx_rcc_driver.h"
+#include "stm32f446xx_i2c_driver.h"
+#include "stm32f446xx_spi_driver.h"
+
+
 #endif /* INC_STM32F446XX_H_ */
