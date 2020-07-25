e0ca705942d9e30211d2f4ec06b5bfb6a941f5af
Rajssss <sssraj.sssraj@gmail.com>
2020-07-25 12:22:47 +0530

    USART Driver Update: Added Basic Handler Definitions (part-1)
    -> Peripheral Control handler
    -> peripheral Clock Control handler
    -> Fixed a small derp ;)
    
    Signed-off-by: Rajssss <sssraj.sssraj@gmail.com>

--- stm32f4xx_driver/driver/src/stm32f446xx_usart_driver.c
+++ stm32f4xx_driver/driver/src/stm32f446xx_usart_driver.c
@@ -19,11 +19,125 @@
  *
  *  Created on: 21-Jul-2020
  *      Author: Rajssss@GitHub.com
- *      Description: I2C Driver for STM32F446RE-Nucleo Board
+ *      Description: USART/UART Driver for STM32F446RE-Nucleo Board
  *
  *      This file contains:
  *           - Function definitions to perform various operation on USART/UART Peripheral.
  *
  *
  */
+
+//includes
+#include "stm32f446xx_usart_driver.h"
+
+
+
+/***********************************************************************************
+ * 					 	USART/UART Clock Control Handler
+ *
+ * @fn: 		- 	USARTx_PeriClkControl
+ *
+ * @brief		-	This function Enables or Disables the peripheral clock of
+ * 					the given USART/UART Peripheral
+ *
+ * @param[1]	-	Base Address of the USART/UART Peripheral
+ *
+ * @param[2]	-	Control value: ENABLE or DISABLE
+ *
+ * @return		-	void
+ *
+ * @Note		-	none
+ *
+ */
+void USARTx_PeriClkControl(USARTx_RegDef_ty *pUSARTx, uint8_t Control)
+{
+	if(Control == ENABLE)
+	{
+		if(pUSARTx == USART1)
+		{
+			USART1_PCLK_EN();
+		}
+		else if(pUSARTx == USART2)
+		{
+			USART2_PCLK_EN();
+		}
+		else if(pUSARTx == USART3)
+		{
+			USART3_PCLK_EN();
+		}
+		else if(pUSARTx == UART4)
+		{
+			UART4_PCLK_EN();
+		}
+		else if(pUSARTx == UART5)
+		{
+			UART5_PCLK_EN();
+		}
+		else if(pUSARTx == USART6)
+		{
+			USART6_PCLK_EN();
+		}
+	}
+	else
+	{
+		if(pUSARTx == USART1)
+		{
+			USART1_PCLK_DI();
+		}
+		else if(pUSARTx == USART2)
+		{
+			USART2_PCLK_DI();
+		}
+		else if(pUSARTx == USART3)
+		{
+			USART3_PCLK_DI();
+		}
+		else if(pUSARTx == UART4)
+		{
+			UART4_PCLK_DI();
+		}
+		else if(pUSARTx == UART5)
+		{
+			UART5_PCLK_DI();
+		}
+		else if(pUSARTx == USART6)
+		{
+			USART6_PCLK_DI();
+		}
+	}
+
+}
+
+
+
+
+/***********************************************************************************
+ * 					 	USART/UART peripheral Control Handler
+ *
+ * @fn: 		- 	USARTx_PeripheralControl
+ *
+ * @brief		-	This function Enable or Disable the given USART/UART Peripheral
+ *
+ * @param[1]	-	Base Address of the USART/UART Peripheral
+ *
+ * @param[2]	-	Control value: ENABLE or DISABLE
+ *
+ * @return		-	void
+ *
+ * @Note		-	none
+ *
+ */
+void USARTx_PeripheralControl(USARTx_RegDef_ty *pUSARTx, uint8_t Control)
+{
+	if(Control == ENABLE)
+	{
+		pUSARTx->CR1 |= (1 << USART_CR1_UE);
+	}
+	else
+	{
+		pUSARTx->CR1 &= ~(1 << USART_CR1_UE);
+	}
+}
+
+
 
